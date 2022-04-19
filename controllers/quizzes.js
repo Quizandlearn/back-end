d8ebd4faa1866d9b025ac1894ea8e99c849bf3ab
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-04-19 17:10:49 +0200

    delete Category model that isn't used

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -1,15 +1,11 @@
-const Quiz = require('../models/quiz');
-const Category = require('../models/category');
+const Quiz = require("../models/quiz");
 
 exports.postQuiz = async (req, res, next) => {
-
-    const createQuiz = await new Quiz({...req.body})
-    if(createQuiz){
-      res.status(200).send({ message: 'Quiz créé' });
-      createQuiz.save();
-    }
-    else{
-      res.status(400).send({ message: 'Les paramètres ne sont pas valides' });
-    }
+  const createQuiz = await new Quiz({ ...req.body });
+  if (createQuiz) {
+    res.status(200).send({ message: "Quiz créé" });
+    createQuiz.save();
+  } else {
+    res.status(400).send({ message: "Les paramètres ne sont pas valides" });
   }
-  
+};
