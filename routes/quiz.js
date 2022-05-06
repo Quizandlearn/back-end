340def20c5caab8d71b7e9f46ce39e5764eaf908
margaux.lenne <margaux.lenne@manomano.com>
2022-05-06 10:56:28 +0200

    Add Route to GET Quizzes

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const quizCtrl = require('../controllers/quizzes');
 
 router.post('/', quizCtrl.postQuiz);
+router.get('/', quizCtrl.getQuiz);
 
 module.exports = router;
