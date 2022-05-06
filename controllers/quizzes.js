6aa352e6d67a5d74e4572f1b17b2c6be7a14b005
margaux.lenne <margaux.lenne@manomano.com>
2022-05-06 10:56:28 +0200

    Add Route to GET Quizzes

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -4,8 +4,17 @@ exports.postQuiz = async (req, res, next) => {
   const createQuiz = await new Quiz({ ...req.body });
   if (createQuiz) {
     res.status(200).send({ message: "Quiz créé" });
-    createQuiz.save();
+    await createQuiz.save();
   } else {
     res.status(400).send({ message: "Les paramètres ne sont pas valides" });
   }
 };
+
+exports.getQuiz = async (req, res, next) => {
+  const quizzes =  await Quiz.find()
+  if (quizzes) {
+    res.status(200).send(quizzes);
+  } else {
+    res.status(400).send({ message: 'Quizzes non trouvées' });
+  }
+}
