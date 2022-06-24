88710ff5a08d93c6340bb25bd92f4ade48df693f
margaux.lenne <margaux.lenne@manomano.com>
2022-06-24 17:04:56 +0200

    Fix Get Category to send also ids

--- controllers/questions.js
+++ controllers/questions.js
@@ -4,12 +4,12 @@ const Quiz = require('../models/quiz');
 exports.postQuestions = async (req, res) => {
   const idQuestionsArray = [];
   try {
-    req.body.questions.map(async (question, i) => {
+    req.body.questions.map(async (question) => {
       const createQuestion = await new Question({ ...question });
       await createQuestion.save();
       await Quiz.updateOne({ _id: question.id_quiz }, { $push: { questions: createQuestion._id } });
       idQuestionsArray.push(createQuestion._id);
-      if (req.body.questions.length === i + 1) {
+      if (req.body.questions.length === idQuestionsArray.length) {
         res.status(200).send({
           message: 'Question(s) créée(s) en bdd et reliée(s) au quiz',
           idQuestions: idQuestionsArray,
