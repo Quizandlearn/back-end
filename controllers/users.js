1238b8746b0e73efa03c3b17eaf215ae0511c115
Marie Flores <marieflores.contact@gmail.com>
2022-06-25 19:56:23 +0200

    Add an else condition in login function

--- controllers/users.js
+++ controllers/users.js
@@ -25,25 +25,25 @@ exports.signup = (req, res) => bcrypt.hash(req.body.password, 10)
 exports.login = (req, res) => User.findOne({ email: req.body.email })
   .then((user) => {
     if (!user) {
-      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      res.status(401).json({ error: 'Utilisateur non trouvé !' });
     }
     bcrypt.compare(req.body.password, user.password)
       .then((valid) => {
         if (!valid) {
-          return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          res.status(401).json({ error: 'Mot de passe incorrect !' });
+        } else {
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              {
+                userId: user._id,
+              },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' },
+            ),
+          });
         }
-        res.status(200).json({
-          userId: user._id,
-          token: jwt.sign(
-            {
-              userId: user._id,
-            },
-            'RANDOM_TOKEN_SECRET',
-            { expiresIn: '24h' },
-          ),
-        });
-      })
-      .catch((error) => {
+      }).catch((error) => {
         res.status(500).json({ error });
       });
   })
