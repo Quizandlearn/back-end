e74ed8a99f23c2ef71b1d6f86b5e85caff2e8221
AlinaSwile <alina.budukina@swile.co>
2022-08-05 15:49:25 +0200

    fix: post bad merge

--- controllers/users.js
+++ controllers/users.js
@@ -2,34 +2,37 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-exports.signup = (req, res) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: hash,
+exports.signup = (req, res) => bcrypt.hash(req.body.password, 10)
+  .then((hash) => {
+    const user = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: hash,
+    });
+    user.save()
+      .then(() => {
+        res.status(201).json({ message: 'Utilisateur créé !' });
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
       });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
 
-exports.login = (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      return bcrypt.compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          return res.status(200).json({
+exports.login = (req, res) => User.findOne({ email: req.body.email })
+  .then((user) => {
+    if (!user) {
+      res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    bcrypt.compare(req.body.password, user.password)
+      .then((valid) => {
+        if (!valid) {
+          res.status(401).json({ error: 'Mot de passe incorrect !' });
+        } else {
+          res.status(200).json({
             userId: user._id,
             token: jwt.sign(
               {
@@ -39,22 +42,12 @@ exports.login = (req, res) => {
               { expiresIn: '24h' },
             ),
           });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
-
-exports.getUser = async (req, res) => {
-  await User.findById(req.params.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      return res.status(200).send(user);
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
+        }
+      }).catch((error) => {
+        res.status(500).json({ error });
+      });
+  })
+  .catch((error) => res.status(500).json({ error }));
 
 exports.modifyUser = async (req, res) => {
   const userToModify = new User({
