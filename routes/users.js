87816b016ead05d86235ef3e2254ba9dada858d3
AlinaSwile <alina.budukina@swile.co>
2022-08-05 14:19:36 +0200

    fix: add auth middleware for user endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/users');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.get('/user/:id', userCtrl.getUser);
-router.put('/user/:id', userCtrl.modifyUser);
+router.get('/user/:id', auth, userCtrl.getUser);
+router.put('/user/:id', auth, userCtrl.modifyUser);
 
 module.exports = router;
