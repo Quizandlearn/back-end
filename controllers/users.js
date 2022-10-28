0a376870887d738b52b2259a56dbe722fb427d79
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-10-28 11:55:07 +0200

    feat: current password validation added

--- controllers/users.js
+++ controllers/users.js
@@ -1,55 +1,59 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-exports.signup = (req, res) => bcrypt.hash(req.body.password, 10)
-  .then((hash) => {
-    const user = new User({
-      name: req.body.name,
-      surname: req.body.surname,
-      email: req.body.email,
-      password: hash,
-    });
-    user.save()
-      .then(() => {
-        res.status(201).json({ message: 'Utilisateur créé !' });
-      })
-      .catch((error) => {
-        res.status(400).json({ error });
+exports.signup = (req, res) =>
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: hash,
       });
-  })
-  .catch((error) => {
-    res.status(500).json({ error });
-  });
 
-exports.login = (req, res) => User.findOne({ email: req.body.email })
-  .then((user) => {
-    if (!user) {
-      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-    }
-    return bcrypt.compare(req.body.password, user.password)
-      .then((valid) => {
-        if (!valid) {
-          return res.status(401).json({ error: 'Mot de passe incorrect !' });
-        }
-        return res.status(200).json({
-          userId: user._id,
-          token: jwt.sign(
-            { userId: user._id },
-            'RANDOM_TOKEN_SECRET',
-            { expiresIn: '24h' },
-          ),
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "Utilisateur créé !" });
+        })
+        .catch((error) => {
+          res.status(400).json({ error });
         });
-      })
-      .catch((error) => res.status(500).json({ error }));
-  })
-  .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+
+exports.login = (req, res) =>
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      return bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          return res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 
 exports.getUser = async (req, res) => {
   await User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
       }
       return res.status(200).send(user);
     })
@@ -78,20 +82,40 @@ exports.modifyUser = async (req, res) => {
 };
 
 exports.modifyPassword = async (req, res) => {
-  bcrypt.hash(req.body.newPassword, 10).then((hash) => {
-    const passwordToUpdate = new User({
-      _id: req.params.id,
-      password: hash,
-    });
+  const user = await User.findOne({ _id: req.params.id });
 
-    User.updateOne({ _id: req.params.id }, passwordToUpdate)
-      .then(() => {
-        res.status(200).json({
-          message: "Password updated successfully!",
-        });
-      })
-      .catch((error) => {
-        res.status(400).json({ error });
+  async function passwordValidation() {
+    try {
+      const matchPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!matchPassword) {
+        return res.status(401).json({ error: "Mot de passe incorrect !" });
+      }
+      return true;
+    } catch {
+      (error) => res.status(500).json({ error });
+    }
+  }
+
+  const isValidPassport = await passwordValidation();
+
+  if (isValidPassport === true) {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+
+      const passwordToUpdate = new User({
+        _id: req.params.id,
+        password: hashedPassword,
       });
-  });
+
+      await User.updateOne({ _id: req.params.id }, passwordToUpdate);
+      return res
+        .status(200)
+        .json({ message: "Mot de passe a été mis a jour!" });
+    } catch {
+      (error) => res.status(400).json({ error });
+    }
+  }
 };
