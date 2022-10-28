83faf0affb9fe3268c4842468e9ef054550172d4
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-10-28 13:09:48 +0200

    fix: change double quotes to single quotes

--- controllers/users.js
+++ controllers/users.js
@@ -1,59 +1,56 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-exports.signup = (req, res) =>
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: hash,
-      });
-
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "Utilisateur créé !" });
-        })
-        .catch((error) => {
-          res.status(400).json({ error });
-        });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
+exports.signup = (req, res) => bcrypt
+  .hash(req.body.password, 10)
+  .then((hash) => {
+    const user = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: hash,
     });
+    user
+      .save()
+      .then(() => {
+        res.status(201).json({ message: 'Utilisateur créé !' });
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
+      });
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
 
-exports.login = (req, res) =>
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
-      }
-      return bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
-          }
-          return res.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+exports.login = (req, res) => User.findOne({ email: req.body.email })
+  .then((user) => {
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    return bcrypt
+      .compare(req.body.password, user.password)
+      .then((valid) => {
+        if (!valid) {
+          return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+        return res.status(200).json({
+          userId: user._id,
+          token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+            expiresIn: '24h',
+          }),
+        });
+      })
+      .catch((error) => res.status(500).json({ error }));
+  })
+  .catch((error) => res.status(500).json({ error }));
 
 exports.getUser = async (req, res) => {
   await User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
       return res.status(200).send(user);
     })
@@ -71,7 +68,7 @@ exports.modifyUser = async (req, res) => {
   await User.updateOne({ _id: req.params.id }, userToModify)
     .then(() => {
       res.status(200).json({
-        message: "User updated successfully!",
+        message: 'User updated successfully!',
       });
     })
     .catch((error) => {
@@ -88,10 +85,10 @@ exports.modifyPassword = async (req, res) => {
     try {
       const matchPassword = await bcrypt.compare(
         req.body.password,
-        user.password
+        user.password,
       );
       if (!matchPassword) {
-        return res.status(401).json({ error: "Mot de passe incorrect !" });
+        return res.status(401).json({ error: 'Mot de passe incorrect !' });
       }
       return true;
     } catch {
@@ -113,7 +110,7 @@ exports.modifyPassword = async (req, res) => {
       await User.updateOne({ _id: req.params.id }, passwordToUpdate);
       return res
         .status(200)
-        .json({ message: "Mot de passe a été mis a jour!" });
+        .json({ message: 'Mot de passe a été mis a jour!' });
     } catch {
       (error) => res.status(400).json({ error });
     }
